3787333694fb58220d50435ce4c0c1d3a8073540
Gustavoph <gusta.coliveira07@gmail.com>
2023-05-11 09:54:22 -0300

    feat: add path alias and theme

--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import { ThemeProvider } from 'styled-components'
-import { THEME } from './src/libs/styled-components'
+
+import { Home } from '@screens/Home'
+import defaultTheme from '@themes/default'
 
 export default function App() {
   return (
-    <ThemeProvider theme={THEME}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
     </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ src/themes/default.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+
+    PURPLE: '#8284FA',
+    PURPLE_DARK: '#5E60CE',
+    BLUE: '#4EA8DE',
+    BLUE_DARK: '#1E6F9F',
+
+    RED: '#F75A68',
+    RED_DARK: '#AA2834',
+
+    GRAY_700: '#0D0D0D',
+    GRAY_600: '#1A1A1A',
+    GRAY_500: '#262626',
+    GRAY_400: '#333333',
+    GRAY_300: '#808080',
+    GRAY_200: '#D9D9D9',
+    GRAY_100: '#F2F2F2',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold',
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+}
